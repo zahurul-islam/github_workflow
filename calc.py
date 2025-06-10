00f99357c6fc74e288d6d0038ae6457dc50369ef
Zahurul Islam <zaisbd@gmail.com>
2025-06-10 14:57:00 +0200

    workflow file updated

--- calc.py
+++ calc.py
@@ -1,19 +1,18 @@
-# write a basic calculator with functions add, substruct, multipy and devide (also check devide by zero)
+# write a basic calculator with functions add, subtract, multiply and divide (also check divide by zero)
 def add(a, b):
     return a + b
 
 
-def substruct(a, b):
+def subtract(a, b):
     return a - b
 
 
-def multipy(a, b):
+def multiply(a, b):
     return a * b
 
 
-def devide(a, b):
+def divide(a, b):
     if b == 0:
-        return "Error: Devide by zero"
+        raise ZeroDivisionError("Error: Divide by zero")
     else:
         return a / b
-
